cc96df3231e60b7db54a5880cfb38344b83c7d65
rynx07 <yuzon.russell07@gmail.com>
2024-01-05 19:12:52 +0800

    Added
    - Dashboard
    - Update the Schema.Prisma
    - Added Buyer's earnings
    - Added Seller's Money Deduction
    - Update App.js

--- audiohive-backend-main/app.js
+++ audiohive-backend-main/app.js
@@ -3,7 +3,6 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import createError from 'http-errors';
 import path from 'path';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
@@ -11,7 +10,8 @@ import { fileURLToPath } from 'url';
 import usersRouter from './routes/users.js';
 import postsRouter from './routes/posts.js';
 import ticketRouter from './routes/ticket.js';
-import ecommerceRouter from './routes/ecommerce.js'; 
+import ecommerceRouter from './routes/ecommerce.js';
+import dashboardRouter from './routes/dashboard.js'; // Import the new dashboard router
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,7 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/tickets', ticketRouter);
-app.use('/api/ecommerce', ecommerceRouter); 
+app.use('/api/ecommerce', ecommerceRouter);
+app.use('/api/dashboard', dashboardRouter); // Add the new dashboard router
 
 const server = app.listen(port, () => console.log(`🚀 Server is running. Server: ${port}`));
 
